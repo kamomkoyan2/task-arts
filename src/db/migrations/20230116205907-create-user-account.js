0283a922c116f9e280e926bf8bf2ca332adcba08
Kamo Mkoyan <kamo_mkoyan>
2023-01-17 01:35:16 +0400

    add User migration model and UserAccount migration and model

---
+++ src/db/migrations/20230116205907-create-user-account.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.BIGINT
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_accounts');
+  }
+};
---
+++ src/db/models/User.ts
@@ -0,0 +1,65 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../database";
+interface UserAttributes {
+    id?: number,
+    email?: string | null,
+    password?: string | null,
+    accessToken?: string | null,
+    verified?: boolean | null,
+    active?: boolean | null,
+
+    createdAt?: Date,
+    updatedAt? : Date
+}
+
+export interface UserInput extends Optional<UserAttributes, 'id'>{ }
+export interface UserOutput extends Required<UserAttributes>{ }
+
+class User extends Model<UserAttributes, UserInput> implements UserAttributes {
+    public id!: number;
+    public name!: string;
+    public email!: string;
+    public password!: string;
+    public accessToken!: string;
+    public verified!: boolean;
+    public active!: boolean;
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt! : Date;
+}
+
+
+User.init({
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    password: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    accessToken: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    verified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    },
+    active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    }
+}, {
+    timestamps: true,
+    sequelize: sequelize,
+    underscored: false
+});
+
+export default User;
